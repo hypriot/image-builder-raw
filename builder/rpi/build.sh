d135a9f308a61559b6b1c99f6b75b3b1a37e62b6
Dieter Reuter <dieter.reuter@me.com>
2016-02-12 15:41:38 +0100

    Shellchecked

--- builder/rpi/build.sh
+++ builder/rpi/build.sh
@@ -9,8 +9,8 @@ fi
 ### setting up some important variables to control the build process
 BUILD_RESULT_PATH="/workspace"
 IMAGE_PATH="rpi-raw.img"
-SD_CARD_SIZE="1200"
-BOOT_PARTITION_SIZE="64"
+SD_CARD_SIZE=1200
+BOOT_PARTITION_SIZE=64
 
 # create empty BOOT/ROOTFS image file
 # - SD_CARD_SIZE in MByte
@@ -18,10 +18,10 @@ BOOT_PARTITION_SIZE="64"
 # - sector block size is 512Bytes
 # - MBR size is 512 Bytes, so we start at sector 2048 (1MByte reserved space)
 BOOTFS_START=2048
-BOOTFS_SIZE=$(expr ${BOOT_PARTITION_SIZE} \* 2048)
-ROOTFS_START=$(expr ${BOOTFS_SIZE} + ${BOOTFS_START})
-SD_MINUS_DD=$(expr ${SD_CARD_SIZE} \* 1024 \* 1024 - 256)
-ROOTFS_SIZE=$(expr ${SD_MINUS_DD} / 512 - ${ROOTFS_START})
+BOOTFS_SIZE=$((BOOT_PARTITION_SIZE * 2048))
+ROOTFS_START=$((BOOTFS_SIZE + BOOTFS_START))
+SD_MINUS_DD=$((SD_CARD_SIZE * 1024 * 1024 - 256))
+ROOTFS_SIZE=$((SD_MINUS_DD / 512 - ROOTFS_START))
 
 dd if=/dev/zero of=${IMAGE_PATH} bs=1MiB count=${SD_CARD_SIZE}
 
@@ -30,7 +30,7 @@ DEVICE=$(losetup -f --show ${IMAGE_PATH})
 echo "Image ${IMAGE_PATH} created and mounted as ${DEVICE}."
 
 # create partions
-sfdisk --force ${DEVICE} <<PARTITION
+sfdisk --force "${DEVICE}" <<PARTITION
 unit: sectors
 
 /dev/loop0p1 : start= ${BOOTFS_START}, size= ${BOOTFS_SIZE}, Id= c
@@ -39,8 +39,8 @@ unit: sectors
 /dev/loop0p4 : start= 0, size= 0, Id= 0
 PARTITION
 
-losetup -d $DEVICE
-DEVICE=`kpartx -va ${IMAGE_PATH} | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+losetup -d "${DEVICE}"
+DEVICE=$(kpartx -va ${IMAGE_PATH} | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1)
 dmsetup --noudevsync mknodes
 BOOTP="/dev/mapper/${DEVICE}p1"
 ROOTP="/dev/mapper/${DEVICE}p2"
@@ -50,8 +50,8 @@ DEVICE="/dev/${DEVICE}"
 sleep 3
 
 # create file systems
-mkfs.vfat ${BOOTP} -n HypriotOS
-mkfs.ext4 ${ROOTP} -L root -i 4096 # create 1 inode per 4kByte block (maximum ratio is 1 per 1kByte)
+mkfs.vfat "${BOOTP}" -n HypriotOS
+mkfs.ext4 "${ROOTP}" -L root -i 4096 # create 1 inode per 4kByte block (maximum ratio is 1 per 1kByte)
 
 echo "### remove dev mapper devices for image partitions"
 kpartx -vds ${IMAGE_PATH} || true
